7d014fee2d6d3527ca32bae2d5f70af08e4a6e6d
don undeen <donundeen@yahoo.com>
2024-12-11 22:38:12 -0500

    new notes

--- bin/installservice.sh
+++ bin/installservice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo cp /home/pi/owliday2024/units/owliday2024.service /lib/systemd/user/
+sudo cp /home/pi/owliday2024/units/owlidays2024.service /lib/systemd/user/
 sudo systemctl daemon-reload
 systemctl --user enable owliday2024.service 
